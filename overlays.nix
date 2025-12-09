e11ed2714cc9f6e126a536b44a642336ce911e32
duyngb <sau.nguyen@outlook.com>
2025-12-09 20:34:38 +0700

    pkgs(devenv): 1.11.1 -> 1.11.2

--- overlays.nix
+++ overlays.nix
@@ -24,6 +24,7 @@ in
 
     {
       inherit inputs;
+      stable = prev;
     }
 
     // (additions final prev)
---
+++ pkgs/devenv.nix
@@ -0,0 +1,24 @@
+{
+  fetchFromGitHub,
+  rustPlatform,
+  stable,
+}:
+
+stable.devenv.overrideAttrs (final: rec {
+  version = "1.11.2";
+
+  src = fetchFromGitHub {
+    owner = "cachix";
+    repo = "devenv";
+    tag = "v${version}";
+    hash = "sha256-8Ivbm9ltg0hUGQYMuRDOI8hbHUzqB9xKZ9ubKAzzwE8=";
+  };
+
+  cargoHash = "sha256-mMmobDZeNqrByowwrDXojVnHeUyC/YbhERpF8iOCZ0s=";
+
+  cargoDeps = rustPlatform.fetchCargoVendor {
+    inherit src;
+    hash = cargoHash;
+  };
+
+})
