046e79aaf13b8355d0877b62ef334bd2673613e8
duyngb <sau.nguyen@outlook.com>
2025-12-20 14:36:47 +0700

    pkgs(otel-tui): 0.5.6 -> 0.6.2

--- pkgs/otel-tui.nix
+++ pkgs/otel-tui.nix
@@ -1,33 +1,32 @@
 {
   lib,
-  buildGo125Module,
+  buildGoModule,
   fetchFromGitHub,
 }:
 
-# using go 1.25 builder, as nixpkgs stable is stuck at 1.24.6, needs 1.24.7
-buildGo125Module (finalAttrs: {
+buildGoModule (finalAttrs: {
   meta = {
     description = "A terminal OpenTelemetry viewer inspired by otel-desktop-viewer";
     homepage = "https://github.com/ymtdzzz/otel-tui";
     license = lib.licenses.asl20;
+    mainProgram = "otel-tui";
   };
 
   pname = "otel-tui";
-  version = "0.5.6";
+  version = "0.6.2";
 
   src = fetchFromGitHub {
     owner = "ymtdzzz";
     repo = "otel-tui";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-x23M69Migk822OIsWnbK2LlfIeBs4//qwJMCRJ0HY+g=";
+    hash = "sha256-uR2E7I+aX7f+efwMTqWBRL3JYvlqnoHPCXZ61MDq+uQ=";
   };
 
   ldFlags = [
-    "-s"
-    "-w"
     "-X main.version=${finalAttrs.version}"
   ];
 
+  doCheck = false;
   doInstallCheck = false;
   env.CGO_ENABLED = "0";
 
@@ -43,7 +42,7 @@ buildGo125Module (finalAttrs: {
   #
   # This vendor hash was obtained with GOWORK=off
   #
-  vendorHash = "sha256-GSPCa4rVSTqQfalcWntCDxYhgtNw0S80wFB6AnbbrQQ=";
+  vendorHash = "sha256-ruU6t7fZrB+sap9VKPTO/qhgEORQ3R8pddW04j4vcik=";
   env.GOWORK = "off";
   # ---------------------------------------------------------------------------
 })
