71a1c8cc4b26daa3d788a2b2b5057604211b319a
duyngb <sau.nguyen@outlook.com>
2026-01-11 19:49:58 +0700

    fix: overrideAttrs use weird 'final'

--- pkgs/gemini-cli-bin.nix
+++ pkgs/gemini-cli-bin.nix
@@ -4,10 +4,10 @@
   fetchurl,
 }:
 
-stable.gemini-cli-bin.overrideAttrs (final: {
+stable.gemini-cli-bin.overrideAttrs (final: rec {
   version = "0.23.0";
   src = fetchurl {
-    url = "https://github.com/google-gemini/gemini-cli/releases/download/v${final.version}/gemini.js";
-    hash = "sha256-TKuI7UC9ofUzRZFqdjY2LEiagjdE2IC471iXkhUCAyw=";
+    url = "https://github.com/google-gemini/gemini-cli/releases/download/v${version}/gemini.js";
+    hash = "sha256-9ZWdPHOCVpxZLM2c5tNQDFDs8RRlj5duUBAVRYRjg+E=";
   };
 })
