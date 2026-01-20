78e62125ef30ddc543eeaa4c0dfd289a764f3d6d
duyngb <sau.nguyen@outlook.com>
2026-01-20 21:15:24 +0700

    pkgs(jjui): 0.9.9 -> 0.9.10

--- pkgs/jjui.nix
+++ pkgs/jjui.nix
@@ -4,12 +4,12 @@
 }:
 
 stable.jjui.overrideAttrs (finalAttrs: rec {
-  version = "0.9.9";
+  version = "0.9.10";
   src = fetchFromGitHub {
     owner = "idursun";
     repo = "jjui";
     tag = "v${version}";
-    hash = "sha256-kBhEX6k/1vVhSqKeUcIQp6sOpKDnJfgUNKOTzbjG/VI=";
+    hash = "sha256-Hsuyf5VcSZcNi2gmubXS47uRarL17oPEtorisY75bbM=";
   };
   vendorHash = "sha256-jte0g+aUiGNARLi8DyfsX6wYYJnodHnILzmid6KvMiA=";
 
