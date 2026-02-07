7ec3c426df46dd759cc6dda4866fc11e5931ef34
duyngb <sau.nguyen@outlook.com>
2026-02-07 15:16:15 +0700

    pkgs(jujutsu): 0.37.0 -> 0.38.0

--- pkgs/jujutsu.nix
+++ pkgs/jujutsu.nix
@@ -5,16 +5,16 @@
 }:
 
 (stable.jujutsu.override { inherit rustPlatform; }).overrideAttrs (final: rec {
-  version = "0.37.0";
+  version = "0.38.0";
 
   src = fetchFromGitHub {
     owner = "jj-vcs";
     repo = "jj";
     tag = "v${version}";
-    hash = "sha256-KN8hukahS+/RowmQoaUrfxCKFT9YUhaIXzh84cBV1Ck=";
+    hash = "sha256-FXbQyKmh/CG/TsRi9yq9L8bVbQ39u6PRLZIet3d/bjA=";
   };
 
-  cargoHash = "sha256-rk3Qft9g67rG0m6rPd4d++wDM5RiRrJAIHdUOQhn9bg=";
+  cargoHash = "sha256-VvgGnAPCvcBmbP2Aw7tdJcjfo+011u4Pqx1pKiiWISA=";
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit src;
