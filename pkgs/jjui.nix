60f06669a5366c68e4586927f7d903b9417d133e
duyngb <sau.nguyen@outlook.com>
2026-09-12 09:27:04 +0700

    jjui: 0.10.9 → 0.10.10

--- pkgs/jjui.nix
+++ pkgs/jjui.nix
@@ -1,17 +1,49 @@
 {
+  lib,
+  buildGo127Module,
   fetchFromGitHub,
-  stable,
+  nix-update-script,
+  versionCheckHook,
+  stdenv,
 }:
+buildGo127Module (final: {
+  pname = "jjui";
+  version = "0.10.10";
 
-stable.jjui.overrideAttrs (finalAttrs: rec {
-  version = "0.10.9";
   src = fetchFromGitHub {
     owner = "idursun";
     repo = "jjui";
-    tag = "v${version}";
-    hash = "sha256-D/ZBH9bsiWO26+xxixD8RKgnoA3x74YdYAIoRznsBTQ=";
+    tag = "v${final.version}";
+    hash = "sha256-bNwWbQq76RztLIiu/uYtHwRvg6H3x59ASCiFRKBib04=";
   };
-  vendorHash = "sha256-BldmFVYpRPdnyeswPKGspH4oZ2mjvFS5VbTu3DN5bJg=";
 
-  doCheck = false;
+  vendorHash = "sha256-T+uv54h89ul0O30HXsngUAIEEfD52bS+zZagCpn8JBU=";
+
+  subPackages = [
+    "cmd/jjui"
+  ];
+
+  dontStrip = true;
+  ldflags = [
+    "-s"
+    "-X main.Version=${final.version}"
+  ];
+
+  checkFlags = lib.optionals stdenv.hostPlatform.isDarwin [
+    "-skip=TestServerAskpass"
+  ];
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "-version";
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "TUI for Jujutsu VCS";
+    homepage = "https://github.com/idursun/jjui";
+    changelog = "https://github.com/idursun/jjui/releases/tag/v${final.version}";
+    license = lib.licenses.mit;
+    mainProgram = "jjui";
+  };
 })
