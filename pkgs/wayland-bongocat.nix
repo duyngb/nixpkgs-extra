cd9fe99597e10e89957c705b3d4f64c1a2627e31
duyngb <sau.nguyen@outlook.com>
2026-09-17 20:48:24 +0700

    wayland-bongocat: 1.2.5 → 2.0.2

--- pkgs/wayland-bongocat.nix
+++ pkgs/wayland-bongocat.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "wayland-bongocat";
-  version = "1.2.5";
+  version = "2.0.2";
 
   src = fetchFromGitHub {
     owner = "saatvik333";
     repo = "wayland-bongocat";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-VkBuqmen6s/LDFu84skQ3wOpIeURZ5e93lvAiEdny70";
+    hash = "sha256-JP7Rcwf+3n6IouuGw3VwhUGLpmEo6ou04nLVWZW5TLo=";
   };
 
   strictDeps = true;
@@ -31,7 +31,6 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   buildInputs = [
-    bash
     wayland
     wayland-protocols
   ];
